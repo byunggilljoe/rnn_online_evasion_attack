0c7fc8a943b8d8f61edff891e5065033a59b1ec2
byunggill joe <byunggill.joe@gmail.com>
2022-04-29 18:47:07 +0900

    mortality

--- experiments/0_mortality-train-victim.sh
+++ experiments/0_mortality-train-victim.sh
@@ -1,4 +1,4 @@
 cd ../mimic3_mnist_sentiment
-python online_attack/MNIST_train_victim.py --trial 0
-python online_attack/MNIST_train_victim.py --trial 1
-python online_attack/MNIST_train_victim.py --trial 2
+python online_attack/mortality_train_victim.py --trial 0
+python online_attack/mortality_train_victim.py --trial 1
+python online_attack/mortality_train_victim.py --trial 2
---
+++ experiments/1_mortality-train-predictor.sh
@@ -0,0 +1,2 @@
+cd ../mimic3_mnist_sentiment
+python online_attack/mortality_predictor_train.py --trial 0
